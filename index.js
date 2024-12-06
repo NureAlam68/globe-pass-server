b3c8068d5f0f2e085b86289017696a41149080e1
NureAlam68 <nurealam151068@gmail.com>
2024-12-06 10:10:19 +0600

    get operation for findone data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require("express");
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -32,13 +32,20 @@ async function run() {
 const database = client.db('visaDB');
 const visaCollection = database.collection('visas');
 
-app.get("/visa", async(req, res) => {
+app.get("/visas", async(req, res) => {
     const cursor = visaCollection.find();
     const result = await cursor.toArray();
     res.send(result);
 })
 
-app.post("/visa", async(req, res) => {
+app.get("/visas/:id", async(req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id)};
+  const result = await visaCollection.findOne(query);
+  res.send(result);
+})
+
+app.post("/visas", async(req, res) => {
     const newVisa = req.body;
     const result = await visaCollection.insertOne(newVisa);
     res.send(result)
