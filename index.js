66279090a4464d4f98ca6048a6c142fa174e8bb0
NureAlam68 <nurealam151068@gmail.com>
2024-12-06 12:50:34 +0600

    get data using limit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
@@ -9,9 +9,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8kdu5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,36 +26,39 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-const database = client.db('visaDB');
-const visaCollection = database.collection('visas');
+    const database = client.db("visaDB");
+    const visaCollection = database.collection("visas");
 
-app.get("/visas", async(req, res) => {
-    const cursor = visaCollection.find();
-    const result = await cursor.toArray();
-    res.send(result);
-})
+    app.get("/visas", async (req, res) => {
+      const cursor = visaCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-app.get("/visas/:id", async(req, res) => {
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id)};
-  const result = await visaCollection.findOne(query);
-  res.send(result);
-})
+    app.get("/visas/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await visaCollection.findOne(query);
+      res.send(result);
+    });
 
-app.post("/visas", async(req, res) => {
-    const newVisa = req.body;
-    const result = await visaCollection.insertOne(newVisa);
-    res.send(result)
-})
+    app.get("/latest-visas", async (req, res) => {
+      const cursor = visaCollection.find().sort({ _id: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-
-
-
-
+    app.post("/visas", async (req, res) => {
+      const newVisa = req.body;
+      const result = await visaCollection.insertOne(newVisa);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -66,12 +66,10 @@ app.post("/visas", async(req, res) => {
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-    res.send('GlobePass server is running....')
-})
+  res.send("GlobePass server is running....");
+});
 
 app.listen(port, () => {
-    console.log(`GlobePass server is running on port: ${port}`)
-})
-
+  console.log(`GlobePass server is running on port: ${port}`);
+});
