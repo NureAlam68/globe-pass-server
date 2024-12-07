6caca9cac008152265d349d8795674fbdff4f801
NureAlam68 <nurealam151068@gmail.com>
2024-12-07 15:23:34 +0600

    delete method for my added visa

--- index.js
+++ index.js
@@ -49,11 +49,25 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/visas/email/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const result = await visaCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/visas", async (req, res) => {
       const newVisa = req.body;
       const result = await visaCollection.insertOne(newVisa);
       res.send(result);
     });
+
+    app.delete("/visas/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await visaCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Fetch applications for the logged-in user
     app.get("/applications/:email", async (req, res) => {
